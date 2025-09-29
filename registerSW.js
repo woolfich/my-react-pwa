4ad49044052471bcdba00056b3387387e38fc70e
woolfich <woolfich@users.noreply.github.com>
2025-09-29 10:30:16 +0000

    deploy: 08b4d95cb8dfa6e2e0556f307d2fe7afb221a637

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-react-pwa/sw.js', { scope: '/my-react-pwa/' })})}
